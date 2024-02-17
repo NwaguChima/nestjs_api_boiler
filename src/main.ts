3e3e9382327855d6c04510bec8a731401c4fe9f1
NwaguChima <nwaguchima48@gmail.com>
2024-02-17 09:59:52 +0100

    setup logger service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    //    {
+    //   bufferLogs: true,
+    // }
+  );
+  // using logger globally
+  // app.useLogger(app.get(MyLoggerService));
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3000);
